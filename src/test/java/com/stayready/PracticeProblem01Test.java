40d1f54c3e5928c73a1f01281c4f8249ec31b053
Lissette Garcia <lissettegarcia@Lissettes-MacBook-Air.local>
2020-07-28 11:35:35 -0500

    attempted

--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,23 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PracticeProblem01Test {
+    PracticeProblem01 problem;
+    private static final String input01 = "racecar";
+
+    @Before
+    public void initalize() {
+        problem = new PracticeProblem01();
+    }
+
+    @Test
+    public void palindromTest() {
+        String expected = "r a c e c a r";
+        String actual = problem.Palindrome(input01);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
