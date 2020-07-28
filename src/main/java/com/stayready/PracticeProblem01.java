8824c25ddeeba67a962babb819deb89b9c68f0d0
Juliana Paul <paul.juliana15@gmail.com>
2020-07-28 12:44:14 -0400

    Progress

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,28 @@
 package com.stayready;
 
 public class PracticeProblem01 {
+
+    public boolean isPalindrome(String input){
+
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) != input.charAt(input.length() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Integer getSubPalindroms(String input){
+
+        String str;
+        int substrings = input.length();
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = i + 2; j < input.length(); j++) {
+                str = input.substring(i, j);
+                //
+            }
+        }
+        return substrings;
+
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,27 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PracticeProblem01Test {
+
+    @Test
+    public void isPalindromeTest(){
+        PracticeProblem01 isPalindromeTest = new PracticeProblem01();
+
+        boolean expected = isPalindromeTest.isPalindrome("LOOOL");
+        boolean actual = true;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getSubPalindromicTest(){
+        PracticeProblem01 palindromesTest = new PracticeProblem01();
+        
+        String expected  = "r a c e c a r";
+        String actual = PracticeProblem01.getSubPalindroms();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
