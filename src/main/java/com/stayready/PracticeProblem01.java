c546c9f447b5754fae5828cbf7cee7bc79358894
KJ Witherspoon <50391760+Spooncodes@users.noreply.github.com>
2020-07-28 13:48:58 -0400

    remix with comments

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -3,26 +3,28 @@ package com.stayready;
 public class PracticeProblem01 {
 
     public static void main(String[] args) {
-
         PracticeProblem01 palindrome = new PracticeProblem01();
 
         String input = "r a c e c a r";
 
-        if (palindrome.isPalindrome(input))
+        if (palindrome.isPalindrome(input)) {
             System.out.println("\"" + input + "\" is a palindrome.");
-        else
+        } else {
             System.out.println("\"" + input + "\" is not a palindrome.");
-
-    }
-
-    static boolean isPalindrome(String str) {
-
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
-                return false;
-            }
         }
-        return true;
     }
 
+    // check for palindrome
+    public static boolean isPalindrome(String str) {
+        int begin = 0; // starts from beginning of string
+        int end = str.length() - 1; // starts from end of string
+
+        while (begin < end) { // compare first character to last character
+            if (str.charAt(begin) != str.charAt(end)) // if ending & beginning do not match
+                return false; // return false
+            begin++;
+            end--;
+        }
+        return true; // if ending & beginning match loop inward
+    }
 }
