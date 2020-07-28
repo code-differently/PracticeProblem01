434bef4ba850ccbbe997b9071138ac45737b9bc0
Paola <camachopaola311@gmail.com>
2020-07-28 12:28:00 -0400

    Practice 01

--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,32 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PracticeProblem01Test {
+    @Test
+    public void isPalindromeTest(){
+        //Given
+        boolean expected = true;
+        //When
+        PracticeProblem01 palindrome = new PracticeProblem01();
+        boolean actual = palindrome.isPalindrome("racecar");
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void palindromPartitionsTest(){
+        //Given
+        String expected =
+                "r a c e c a r  " +
+                "\nr aceca r" +
+                "\nr a cec a r";
+
+        //When
+        PracticeProblem01 palindrome = new PracticeProblem01();
+        String actual = palindrome.palindromePartitions("racecar");
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
 }
