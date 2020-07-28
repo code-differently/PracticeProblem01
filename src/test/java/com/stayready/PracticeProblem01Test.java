6da90fd13dc3a3a5671dae513666b8485b363929
esanvee <eesanvee18@students.desu.edu>
2020-07-28 13:29:28 -0400

    Work in progress

--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,62 @@
 package com.stayready;
 
+import org.junit.Test;
+
+import org.junit.Assert;
+import org.junit.Before;
+
 public class PracticeProblem01Test {
+
+    public static PracticeProblem01 practice;
+
+
+    @Test
+
+    public void testPalindrom(){
+
+        
+
+        String text = "raCecAr";
+
+        boolean actual = PracticeProblem01.isPalindrom(text);
+
+        Assert.assertTrue(actual);
+
+    }
+
+    @Test
+
+    public void testPartition(){
+
+        String text = "NOT A PALINDROM";
+
+        String expected = "Impossible! This is not a palindorm!";
+        String actual = PracticeProblem01.partition(text);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void testPartition2(){
+
+        String text = "racecar";
+
+        String expected = "r a c e c a r";
+        String actual = PracticeProblem01.partition(text);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void testAllPartitions(){
+
+        String text = "NOT A PALINDROM";
+
+        String expected = "Impossible! This is not a palindrom!";
+        String actual = PracticeProblem01.allPartitions(text);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
