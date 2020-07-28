337716ca5ab70da6233d7b4eb0221c0bb06f0e81
mercelisvaughan <mercelis.vaughan51@gmail.com>
2020-07-28 12:26:39 -0400

    Progress

--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,30 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PracticeProblem01Test {
+
+    @Test
+    public void isPalimdromeTest(){
+        // Given
+        PracticeProblem01 p = new PracticeProblem01();
+        String input = "racecar";
+        // When
+        Boolean actual = p.isPalindrome(input);
+        // Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void makeItHappenTest(){
+        // Given
+        PracticeProblem01 p = new PracticeProblem01();
+        String input = "racecar";
+        String expected = "r a c e c a r";
+        // When
+        String actual = p.makeItHappen(input);
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
