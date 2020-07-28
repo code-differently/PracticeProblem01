ec537b12d0a0cd1c71abb2e7971cc50b1b45ff78
Dominic Davis <dompc6789@gmail.com>
2020-07-28 14:00:19 -0400

    implemented basic unit tests setup

--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,30 @@
 package com.stayready;
 
-public class PracticeProblem01Test {
+import org.junit.Assert;
+import org.junit.Test;
+public class PracticeProblem01Test {;
+    private static final String input01 = "racecar";
+
+    @Test
+
+    public void getSubPalindromeTest(){
+        //Given
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        //When
+        String expected = "r a c e c a r";
+        String actual = practiceProblem01.getSubPalindromes(input01);
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindromTestTrue(){
+        //Given
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        //When
+        Boolean actual = practiceProblem01.isPalindrome("aba");
+        //Then
+        Assert.assertTrue(actual);
+
+    }
 }
