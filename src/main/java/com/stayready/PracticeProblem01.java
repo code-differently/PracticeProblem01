5cec920ae933efe58272732f51bb830cd903f65c
Kaiyen Thompson <kthompson6300@gmail.com>
2020-07-28 12:35:27 -0400

    can look

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,15 @@
 package com.stayready;
 
 public class PracticeProblem01 {
+
+    public Boolean isPalindrones(String input){
+        return null;
+    }
+
+    public String getSubPalindrones(String input) {
+        return null;
+    }
+
+
 }
+
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,44 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class PracticeProblem01Test {
+    private static final String input01 = "racecar";
+
+    @Test
+    public void getSubPalindronesTest01(){
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String expected = "r a c e c a r";
+        String actual = practiceProblem01.getSubPalindrones(input01);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getSubPalindronesTest02(){
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String expected = "r aceca r";
+        String actual = practiceProblem01.getSubPalindrones(input01);
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void getSubPalindronesTest03(){
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String expected = "r a cec a r";
+        String actual = practiceProblem01.getSubPalindrones(input01);
+        Assert.assertEquals(expected,actual);
+
+    }
+
+
+    @Test
+    public void isPalinedrineTest(){
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        Boolean actual = practiceProblem01.isPalindrones("aba");
+        Assert.assertTrue(actual);
+    }
+
 }
