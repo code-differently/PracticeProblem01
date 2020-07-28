ac6888d0e7df3c54c382c4d0663c8aad13f9aa0b
madlynjoa1 <maddlynjoa1@icloud.com>
2020-07-28 12:28:06 -0400

    polin

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,21 @@
 package com.stayready;
 
 public class PracticeProblem01 {
+
+    public Boolean isPolindrome(String text){
+        StringBuilder output= new StringBuilder("text");
+        StringBuilder reverseWord= output.reverse();
+        return (output.equals(reverseWord))?true:false;
+
+    }
+    //Start partitioning from 1 letter to more checking if polindrome
+    public String partition(String mainText){
+        char[] letters= mainText.toCharArray();
+//        while(isPolindrome()==true){
+//
+//        }
+//
+        return "";
+
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,31 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PracticeProblem01Test {
+    PracticeProblem01 practice1;
+    @Before
+    public void setup(){
+        this.practice1= new PracticeProblem01();
+    }
+    @Test
+    public void isPolindrome(){
+        Boolean expected= true;
+
+        Boolean actual= practice1.isPolindrome("racecar");
+
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void partitionTest01(){
+        String expected= String.format("r"+" "+"aceca"+" "+ "r");
+
+        String actual= practice1.partition("racecar");
+
+        Assert.assertEquals(expected,actual);
+
+    }
 }
