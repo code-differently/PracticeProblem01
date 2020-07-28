903395019a5521b9bb68b32bb415da28c7fcf0f7
girlswhocodee <myekini1@umbc.edu>
2020-07-28 12:26:08 -0400

    Palindrome Practice 1 Attempt

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,41 @@
 package com.stayready;
 
+import java.util.ArrayList;
+
 public class PracticeProblem01 {
-}
+
+    public boolean partitionPalindrome(String input){
+    //Method to check if string is palindrome
+        //input = "n u r s e s r u n";
+        //ArrayList<String> palindrome = new ArrayList<>(list);
+        int leng = input.length();
+        leng--;
+
+        for (int i=0; i < leng; i++)
+        {
+            if(input.charAt(i) != input.charAt(leng))
+                return false;
+                leng--;
+        }
+        return true;
+    }
+
+    public String printPalindromePartition(String input){
+    //Method to return list of possible palindromes
+        ArrayList<ArrayList<String>> storePart = new ArrayList<>();
+        for(ArrayList<String> i: storePart)
+        {
+            for(String j: i){
+                
+            }
+            System.out.println();
+        }
+        return input; 
+    }
+    
+    }
+
+
+
+    
+
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,34 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PracticeProblem01Test {
+
+    @Test
+    public void PartitionPalindromeTest(){
+    //Check if string is a palindrome
+        //Given
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String input = "n u r s e s r u n";
+        boolean expected = true;
+
+        //When
+        boolean actual = practiceProblem01.partitionPalindrome(input);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void PartitionPalindromeTest2(){
+    //test for the return of all possible palindromes in string
+        //Given
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String input = "";
+        
+    }
+    
+    
 }
