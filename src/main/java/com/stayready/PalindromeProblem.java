4e5c4164dccaf207e4aefa7ab962f39b733a2e0b
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-28 16:32:04 -0500

    finished it

--- src/main/java/com/stayready/PalindromeProblem.java
+++ src/main/java/com/stayready/PalindromeProblem.java
@@ -14,31 +14,48 @@ public class PalindromeProblem {
         }
         StringBuilder allPalindromes = new StringBuilder();
         if(isStringPalindrome(input)) {
-            allPalindromes.append(computeIndividualCharacters(input));
             allPalindromes.append(findPalindromeLengthTwoOrMore(input));
         }
         return allPalindromes.toString();
     }
 
-    private String computeIndividualCharacters(String input) {
-        StringBuilder individualCharacters = new StringBuilder();
-        int index = 0;
-        for(Character letter: input.toCharArray()) {
-            individualCharacters.append(index + 1 < input.length() ? letter + " " : letter);
-            index++;
-        }
-        individualCharacters.append("\n");
-        return individualCharacters.toString();
-    }
+//    private String computeCharactersBefore(String input, int howFar) {
+//        StringBuilder individualCharacters = new StringBuilder();
+//        for(int index = 0; index < howFar; index++) {
+//            Character letter = input.charAt(index);
+//            individualCharacters.append(index + 1 < howFar ? letter + " " : letter);
+//        }
+//        return individualCharacters.toString();
+//    }
+//
+//    private String computeCharactersAfter(String input, int howFar) {
+//        StringBuilder individualCharacters = new StringBuilder();
+//        int length = input.length();
+//        for(int index = length - 1; index >= length - howFar; index--) {
+//            Character letter = input.charAt(index);
+//            individualCharacters.append(index + 1 < howFar ? letter + " " : letter);
+//        }
+//        return individualCharacters.toString();
+//    }
 
     private String findPalindromeLengthTwoOrMore(String input) {
-        StringBuilder palindromePartitions = new StringBuilder();
+        input = input.replaceAll("", " ");
+        input = input.replaceFirst(" ", "");
+        input = input.substring(0, input.length() - 1);
+        StringBuilder palindromePartitions = new StringBuilder(input + "\n");
         int start = 1;
         int end = input.length() - 1;
-        while(start != end - 1) {
-            palindromePartitions.append(input.substring(0, start) + " " + input.substring(start, end) + " " + input.substring(end) +"\n");
-            start++;
-            end--;
+        while(start < end) {
+            String beginning = input.substring(0, start);
+            String middle = input.substring(start, end).replaceAll(" ", "");
+            String ending = input.substring(end);
+            String palindromeSubset = beginning + " " + middle + " " + ending +"\n";
+            if(!palindromePartitions.toString().contains(palindromeSubset)) {
+                palindromePartitions.append(palindromeSubset);
+            }
+            //there are spaces in the string
+            start += 2;
+            end -= 2;
         }
         int indexOfLastNewLine = palindromePartitions.lastIndexOf("\n");
         return palindromePartitions.toString().substring(0, indexOfLastNewLine);
