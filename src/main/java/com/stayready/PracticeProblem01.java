26c19a55ff4e3b8562fadb8d85ac8bba81f1f2b4
brianawilliams <williamsb16@comcast.net>
2020-07-28 12:29:53 -0400

    still working

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,37 @@
 package com.stayready;
 
 public class PracticeProblem01 {
+    String word;
+
+    public void PracticeProblem01(String word){
+        this.word = word;
+
+    }
+
+    public boolean isPalindrome(String word){
+        String reversedWord = " ";
+        for(int i = word.length()-1; i >= 0; i--){
+            reversedWord += word.charAt(i);
+        }
+        String newWord = reversedWord.trim();
+        if(word.equals(newWord)){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+
+    public String palindromicPartitions(String word){
+        String moreWords = " ";
+        char[] wordCharacters = word.toCharArray();
+        String newWord = " ";
+        for(int i = 0; i <= wordCharacters.length; i++){
+            moreWords = newWord.substring(i, i+1);
+            moreWords += " ";
+        }
+
+        return moreWords;
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,67 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PracticeProblem01Test {
+    //public static final String word = "racecar";
+
+    @Test
+    public void isPalindromeTest(){
+        //Given
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String expected = "racecar";
+
+        //When
+        boolean actual = practiceProblem01.isPalindrome(expected);
+
+        //Then
+        Assert.assertTrue(expected, actual);
+
+    }
+
+    @Test
+    public void palindromicPartitions01(){
+        //Given
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String expected = "r a c e c a r";
+
+        //When
+        String actual = practiceProblem01.palindromicPartitions(expected);
+
+        //Then
+        Assert.assertEquals(expected,actual);
+
+
+    }
+
+    @Test
+    public void palindromicPartitions02(){
+        //Given
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String expected = "r aceca r";
+
+        //When
+        String actual = practiceProblem01.palindromicPartitions(expected);
+
+        //Then
+        Assert.assertEquals(expected,actual);
+
+
+    }
+
+    @Test
+    public void palindromicPartitions03(){
+        //Given
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String expected = "r a cec a r";
+
+        //When
+        String actual = practiceProblem01.palindromicPartitions(expected);
+
+        //Then
+        Assert.assertEquals(expected,actual);
+
+
+    }
 }
