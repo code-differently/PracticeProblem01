f25d3c792b03f2389ffcb3658a9fd7b48c413361
Francisco Burgos <burgfa@farmingdale.edu>
2020-07-28 12:28:38 -0400

    Ready for review

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,24 @@
 package com.stayready;
 
 public class PracticeProblem01 {
+
+    public Boolean isPalindrom(String word) {
+        StringBuilder reverseWord = new StringBuilder();
+        char[] charactersArr = word.toCharArray();
+        for (int index = 0; index <charactersArr.length; index++) {
+            reverseWord.append(charactersArr[index]).toString();
+        }
+        return (reverseWord.toString()).equals(word);
+    }
+
+    public Boolean checkIfPalindrom(String w) {
+        char[] charsArr = w.toCharArray();
+        StringBuilder palindormWord = new StringBuilder();
+        for(int index = 0; index < charsArr.length; index++) {
+            if (w.substring(0).equals(w.substring(charsArr.length))) {
+                palindormWord.append(charsArr);
+            }
+        }
+        return (palindormWord.toString()).equals(w);
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,62 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PracticeProblem01Test {
+
+    PracticeProblem01 aPracticeProblem;
+    @Before
+    public void setup(){
+        this.aPracticeProblem = new PracticeProblem01();
+    }
+
+    @Test
+    public void isPalindromTest(){
+        // Given
+        Boolean expected = true;
+
+        // When
+        Boolean actual = aPracticeProblem.isPalindrom("racecar");
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindromTest2(){
+        // Given
+        Boolean expected = true;
+
+        // When
+        Boolean actual = aPracticeProblem.isPalindrom("dad");
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindromTest3(){
+        // Given
+        Boolean expected = false;
+
+        // When
+        Boolean actual = aPracticeProblem.isPalindrom("hope");
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkIfPalindromTest(){
+        // Given
+        Boolean expected = true;
+
+        // When
+        Boolean actual = aPracticeProblem.checkIfPalindrom("dad");
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
 }
