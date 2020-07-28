c683be729930b934f479b0d14d96f3e4fa961254
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-28 12:26:24 -0400

    practiceprob01 so far

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,26 @@
 package com.stayready;
 
+import java.util.SplittableRandom;
+
 public class PracticeProblem01 {
+
+    public static Boolean isPalindrome(String input){
+        String rvrsd = "";
+        for(int idx = input.length() - 1; idx >= 0; idx--){
+            rvrsd += input.charAt(idx);
+        }
+
+        if (input.equals(rvrsd)){
+            return true;
+        }
+        return false;
+    }
+
+    public static String getPartitions(String input){
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,51 @@
 package com.stayready;
 
-public class PracticeProblem01Test {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PracticeProblem01Test { // Haha I got my test this time Monsieur Baron Von Codem!!
+
+    private static final String testInput1 = "racecar";
+    private static final String testInput2 = "batman";
+
+    @Test
+    public void getPartitionsTest(){
+        // Given
+        String expected =   "r a c e c a r\n" +
+                            "r aceca r\n" +
+                            "r a cec a r";
+
+        // When
+        String actual = PracticeProblem01.getPartitions(testInput1);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void isPalindromeTest_T(){
+        // Given
+
+        // When
+        Boolean actual = PracticeProblem01.isPalindrome(testInput1);
+
+        // Then
+        Assert.assertTrue(actual);
+
+    }
+
+    @Test
+    public void isPalindromeTest_F() {
+        // Given
+
+        // When
+        Boolean actual = PracticeProblem01.isPalindrome(testInput2);
+
+        // Then
+        Assert.assertFalse(actual);
+
+    }
+
+
 }
