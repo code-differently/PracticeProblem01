626bd4dd3e9c42c86383cd2cc9462393616aade9
CourtneeL <48140227+CourtneeL@users.noreply.github.com>
2020-07-28 12:32:37 -0400

    it is not done yet

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,23 @@
 package com.stayready;
 
-public class PracticeProblem01 {
+public class PracticeProblem01 
+{
+    public boolean isPalindrone(String input)
+    {
+        int x = input.length() - 1;
+
+        char [] letters = input.toCharArray();
+
+        for(int i = 0; i < x; i++)
+        {
+            if(letters[i] != letters[x])
+            {
+                return false;
+            }
+
+            x--;
+        }
+
+        return true;
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,31 @@
 package com.stayready;
 
-public class PracticeProblem01Test {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PracticeProblem01Test 
+{
+    @Test
+    public void constructorTest()
+    {
+        PracticeProblem01 palin = new PracticeProblem01();
+
+        Assert.assertNotNull(palin);
+    }
+
+    @Test 
+    public void isPalindroneTest()
+    {
+        //Given
+        PracticeProblem01 palin = new PracticeProblem01();
+
+        //Then
+        boolean actual = palin.isPalindrone();
+
+        //When
+        assertTrue(actual);
+    }
 }
