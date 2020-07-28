da81778eca83dbec69fc6f5ec5b4cf9ea7785e19
Joshua Bowman <joshuabowman@Joshuas-Air.fios-router.home>
2020-07-28 12:28:46 -0400

    work on problem01

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,51 @@
 package com.stayready;
 
+import java.lang.*;
+
 public class PracticeProblem01 {
+
+    public String isPalindrome(String input){
+            String string = "";
+            String reverseString = new StringBuilder(input).reverse().toString();
+            if (input.equals(reverseString)){
+                string = "True";
+            }else{
+                string = "False";
+            }
+            return string;
+        }
+
+
+    public String firstOutputPalindrome(String input){
+            String string = "racecar";
+            String reverseString = new StringBuilder(input).reverse().toString();
+            if (input.equals(reverseString)){
+                string = string.replaceAll(".", "$0 ");
+            }else{
+                string = "False";
+            }
+        return string;
+    }
+
+    public String secondOutputPalindrome(String input){
+        String string = "racecar";
+        String reverseString = new StringBuilder(input).reverse().toString();
+        if (input.equals(reverseString)){
+
+        }else{
+            string = "False";
+        }
+        return string;
+    }
+
+    public String thirdOutputPalindrome(String input){
+        String string = "racecar";
+        String reverseString = new StringBuilder(input).reverse().toString();
+        if (input.equals(reverseString)){
+
+        }else{
+            string = "False";
+        }
+        return string;
+    }
 }
