39ccdd4628709e10c79244059bcca622fee3e4bc
Solomon Jackson <solomonjackson@solomons-air.myfiosgateway.com>
2020-07-28 12:00:27 -0400

    Problem completed with Tests.

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,68 @@
 package com.stayready;
 
+import java.util.Arrays;
+import java.util.logging.Logger;
+
 public class PracticeProblem01 {
+
+    private static Logger log = Logger.getGlobal();
+
+    public boolean isPalindrome(String input){
+        int count = 0;
+        int s = input.length()-1;
+        char [] letters = input.toCharArray();
+        for(int b = 0; b < s; b++){
+            if(letters[b] != letters[s]){
+                return false;
+            }
+            s--;
+        }
+        return true;
+    }
+
+    public String getPartitions(String input){
+        String partitions = "";
+        if(isPalindrome(input)){
+            for(char l: input.toCharArray()){
+                partitions += l + " ";
+            }
+            partitions = partitions.trim();
+        }else{
+            log.info("This String is not a Palindrome.");
+            partitions = null;
+        }
+        return partitions;
+    }
+
+    public String getFirstPartitions(String input){
+        String partitions = "";
+        if(isPalindrome(input)){
+            if(input.length() < 3){
+                return getPartitions(input);
+            }
+            for(char l: input.toCharArray()){
+                partitions = input.substring(0,1) + " " +input.substring(1, input.length()-1) + " " + input.substring(input.length()-1);
+            }
+        }else{
+            log.info("This String is not a Palindrome.");
+            partitions = null;
+        }
+        return partitions;
+    }
+
+    public String getSecondPartitions(String input){
+        String partitions = "";
+        if(isPalindrome(input)){
+            if(input.length() < 5){
+                return getFirstPartitions(input);
+            }
+            for(char l: input.toCharArray()){
+                partitions = input.substring(0,1) + " " + input.substring(1,2) + " "+input.substring(2, input.length()-2) + " " +input.substring(input.length()-2, input.length()-1)+" "+ input.substring(input.length()-1);
+            }
+        }else{
+            log.info("This String is not a Palindrome.");
+            partitions = null;
+        }
+        return partitions;
+    }
 }
