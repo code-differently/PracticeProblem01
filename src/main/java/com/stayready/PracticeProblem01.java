d138cf6e3f0e965048a57dcffca5f7448cb4cfb8
Brandon Daughtry <bdaughtr@aggies.ncat.edu>
2020-07-28 12:45:05 -0400

    incomplete

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,22 @@
 package com.stayready;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class PracticeProblem01 {
+
+    public static boolean isPalindrone(String word){
+        HashSet<Character> temp = new HashSet<>();
+
+        for (int i=0; i<word.length(); i++){
+            if(!temp.add(word.charAt(i)))
+                temp.remove(word.charAt(i));
+        }
+        if(temp.isEmpty() || temp.size() == 1)
+            return true;
+        else
+            return false;
+    }
+
+
 }
