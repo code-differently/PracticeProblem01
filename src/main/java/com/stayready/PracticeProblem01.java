262a4bbbfa875e74c5413ef125ee2852eaeafd78
Dominique Smith <cyanthecomputerscientist@gmail.com>
2020-07-28 12:26:09 -0400

    one unit test failure

--- src/main/java/com/stayready/PracticeProblem01.java
+++ src/main/java/com/stayready/PracticeProblem01.java
@@ -1,4 +1,52 @@
 package com.stayready;
 
 public class PracticeProblem01 {
+
+    //Step one check if it is a palindrome 
+    public Boolean isPalindrome(String input)
+    {
+        Boolean isPalindrome = true; //assume the string is a palindrome
+        int j = input.length()-1; //variable starting at the end of the string
+        for (int i = 0; i < input.length(); i++)  //Loop to go through the array
+        {
+            if (input.charAt(i) != input.charAt(j)) //if the letter at the end are not the same then the string isn't a palindrome
+            {
+                isPalindrome = false;   
+            }
+            j--; //decrement 
+        }
+        return isPalindrome; 
+    }
+    //step two get all possible substrings of the palindrome
+    public String subStrings (String input)
+    {
+        String substrings = ""; //sorry tariq I know you hate nested for loops 
+        for (int i = 0; i < input.length(); i++)  //loop that goes through the string
+        {
+            for (int j = i+1; j <= input.length(); j++) //loop that is ahead of the one above that goes through string
+            {
+                substrings+= input.substring(i, j); 
+                substrings+="\n";
+            }
+        }
+        return substrings.trim();
+    }
+
+    //step three out put all the substings that are palindrome
+
+    public String palindromePartitions(String input)
+    {
+        String palindromePartition = "";
+        String subStrings = subStrings(input); //get all possible substrings
+        String [] strings = subStrings.split("\n"); //place them into a string array
+        for (int i = 0; i < strings.length; i++) //loop to go through an array
+        {
+            if(isPalindrome(strings[i]) == true) //if the string is a palindrome add it to the set string variable
+            {
+                palindromePartition += strings[i];
+            }
+            palindromePartition += "\n";
+        } 
+        return palindromePartition.trim();
+    }
 }
--- src/test/java/com/stayready/PracticeProblem01Test.java
+++ src/test/java/com/stayready/PracticeProblem01Test.java
@@ -1,4 +1,45 @@
 package com.stayready;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class PracticeProblem01Test {
+public class PracticeProblem01Test 
+{
+    @Test
+    public void isPalindromeTest()
+    {
+        //Given 
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String input = "racecar";
+        //When 
+        Boolean actual = practiceProblem01.isPalindrome(input);
+        Boolean expected = true;
+        //Then 
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void substringTest()
+    {
+        //Given 
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String input = "abbc";
+        //When
+        String actual = practiceProblem01.subStrings(input);
+        String expected = "a\nab\nabb\nabbc\nb\nbb\nbbc\nb\nbc\nc";
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void palindromePartitionsTest()
+    {
+        //Given 
+        PracticeProblem01 practiceProblem01 = new PracticeProblem01();
+        String input = "racecar";
+        //When
+        String actual = practiceProblem01.palindromePartitions(input);
+        String expected = "r a c e c a r\nr aceca r\nr a cec a r\n";
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
